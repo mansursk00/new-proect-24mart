c8304785bc22ecfc6ecb5935d7a0748ee0fae10a
mansursk00 <mnsulnv@gmail.com>
2025-03-27 16:18:41 +0600

    Nav

--- src/widgets/HeaderWidget/HeaderWidget.tsx
+++ src/widgets/HeaderWidget/HeaderWidget.tsx
@@ -8,7 +8,21 @@ export default function HeaderWidget() {
   return (
     <header>
       <div className="container">
-        
+        <div className={styles.head}>
+          <NavLink to='/' className={styles.logo}>
+            <img src="/logo.png" alt="logo" />
+          </NavLink>
+          <label className={styles.searchLabel} htmlFor='search'>
+          </label>
+          <Nav />
+          <div className={styles.btns}>
+            <button className={styles.likeBtn}>
+            </button>
+            <NavLink to={'/cart'} className={styles.cartBtn}>
+            </NavLink>
+          </div>
+
+        </div>
       </div>
     </header>
   )
